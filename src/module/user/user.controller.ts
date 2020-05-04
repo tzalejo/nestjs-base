8cc6551c6e340779e5eb13ea5be1fa73efb0e3f9
tzalejo <tzalejo@gmail.com>
2020-05-04 15:59:19 -0300

    se crea controller con sus metodos

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Patch, Delete, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
+import { User } from './user.entity';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(
+    private readonly _userService: UserService​​
+  ){}
+  // ParseIntPipe es para parsear el valor que viene como un objeto= {id:'1'}
+  // este comportamiento es normal de nodejs, si se quiere parsear los params a un tipo
+  // number se debe crear un middleware. En nest esto se resuelve con un pipe: ParseIntPipe
+  @Get(':id')
+  async getUser(@Param('id', ParseIntPipe) id: number): Promise<UserDto> {
+    const user = await this._userService.get(id);
+    return user;
+  }
+
+  @Get()
+  async getUsers(): Promise<UserDto[]> {
+    const users = await this._userService.getAll();
+    return users;
+  }
+
+  @Post('create')
+  async createUser(@Body() user: User): Promise<UserDto> {
+    const createUser = await this._userService.create(user);
+    return createUser;
+  }
+
+  @Patch(':id')
+  async updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: User): Promise<any> {
+    const updateUser = await this._userService.update(id, user);
+    return updateUser;
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id', ParseIntPipe) id: number) {
+    await this._userService.delete(id);
+    return true;
+  }
+}
