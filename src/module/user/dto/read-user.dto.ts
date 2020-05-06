5152d9359546e630e2b7e7824ffff0bf49323918
tzalejo <tzalejo@gmail.com>
2020-05-06 18:37:39 -0300

    implementando Dto(Role y User)

--- src/module/user/dto/read-user.dto.ts
+++ src/module/user/dto/read-user.dto.ts
@@ -1,18 +1,26 @@
 import { IsNumber, IsEmail, IsString } from "class-validator";
 import { ReadUserDetailDto } from "./read-user-details.dto";
-import { Type } from "class-transformer";
-
+import { Type, Exclude, Expose } from "class-transformer";
+import { ReadRoleDto } from "src/module/role/dto";
+@Exclude()
 export class ReadUserDto {
+  @Expose()
   @IsNumber()
   readonly id: number;
   
+  @Expose()
   @IsString()
   readonly username: string;
   
+  @Expose()
   @IsEmail()
   readonly email: string;
 
+  @Expose()
   @Type(() => ReadUserDetailDto) // para indicar q tipo va a castiar..
   readonly details: ReadUserDetailDto​​;
 
+  @Expose()
+  @Type(() => ReadRoleDto) // para indicar q tipo va a castiar..
+  readonly roles: ReadRoleDto[];
 }
